49b1a3f362a7257b7c31f0c702fcbb15deaace6a
SMBuck <buckm2@g.cofc.edu>
2022-01-19 15:06:52 -0500

    Completed hw 1 assignment

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,51 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Sam Marshall Buck >
+<hw1>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <The computer is able to solve math problems for us, but it takes time to
+figure out how to write out the problem correctly and logically, so
+the program can solve it for us like the exercises we did in the HW.
+Once the problem is solved then the computer is
+able to convert/complete the arithmatic required to get your answer. >
 
 Certification of Authenticity:
 <include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+
 """
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    totalshots = eval(input("Enter the player's total shots: "))
+    shotsmade = eval(input("Enter how many shots the player made: "))
+    shootingpercentage = shotsmade / totalshots * 100
+    print("Shooting Percentage: ", shootingpercentage, "%")
 
 
 def coffee():
-    pass
+    poundsofcoffee = eval(input("How many pounds of coffee would you like? "))
+    totalprice = 10.50 * poundsofcoffee + 0.86 * poundsofcoffee + 1.50
+    print("Your total is: ", totalprice)
 
 
 def kilometers_to_miles():
-    pass
+    kilometerstraveled = eval(input("How many kilometers did you travel? "))
+    mileconversion = kilometerstraveled / 1.61
+    print("That's ", mileconversion, "miles!")
 
 
 if __name__ == '__main__':
