4d589726fe9eb94a86df5fe832179a321e68702c
SMBuck <buckm2@g.cofc.edu>
2022-01-24 14:46:55 -0500

    Completed Lab 1 assignment

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -8,8 +8,8 @@ def monthly_interest():
     step1 = net_balance * billing_cycle
     step2 = payment_amount * payment_made
     step3 = step1 - step2
-    step4 = step3 / billing_cycle
-    step5 = annual_interest / 12
-    step6 = step5 / 100
-    step7 = step4 * step6
-    print("This is the monthly interest charge: $", step7)
+    average_daily_balance = step3 / billing_cycle
+    monthly_conversion = annual_interest / 12
+    percent_decimal = monthly_conversion / 100
+    monthly_interest_charge = average_daily_balance * percent_decimal
+    print("This is the monthly interest charge: $", monthly_interest_charge)
