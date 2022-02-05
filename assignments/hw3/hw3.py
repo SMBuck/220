cd3a4d7163ed6167d3726b008299033745691193
SMBuck <buckm2@g.cofc.edu>
2022-02-04 21:46:46 -0500

    Completed hw3 assignment

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,34 +1,63 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Sam Marshall Buck>
+<hw3>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <This assignment was detailed in making us use an accumulator
+for most of the problems but using for loops as well.I figured it out mostly but
+the last problem i got close but could not figure it out>
 
 Certification of Authenticity:
 <include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is my own work, but I discussed it with: <Bradley>
 """
 
 
+
 def average():
-    pass
+    total_grades = eval(input("how many grades will you enter? "))
+    my_sum = 0
+    for _ in range(1, total_grades + 1):
+        grades = eval(input("Enter grade: "))
+        my_sum = my_sum + grades
+    mean_grade = my_sum / total_grades
+    print("average is", mean_grade)
 
 
 def tip_jar():
-    pass
+    my_sum = 0
+    for _ in range(1, 6):
+        donate = eval(input("how much would you like to donate? "))
+        my_sum = my_sum + donate
+    print("total tips: ", my_sum)
 
 
 def newton():
-    pass
+    variable = eval(input("what number do you want to square root? "))
+    improve = eval(input("how many times should we improve the approximation? "))
+    guess = variable
+    for _ in range(1, improve + 1):
+        guess = (guess + variable/guess)/2
+    print("the square root is approximately", guess)
 
 
 def sequence():
-    pass
+    user_input = eval(input("how many terms would you like? "))
+    for i in range(1, user_input + 1):
+        print((i - 1) + (i % 2), end=" ")
 
 
 def pi():
-    pass
+    user_input = eval(input("how many terms in the series? "))
+    product = 1
+    for i in range(1, user_input + 1):
+        product = (2 * i) / (2 * i - 1) * (2 * i) / (2 * i + 1)
+        final_product = product * product
+        answer = final_product * 2
+        print(answer)
+
+
+
+
 
 
 if __name__ == '__main__':
