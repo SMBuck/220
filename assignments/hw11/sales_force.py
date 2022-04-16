82e6c667e1262143319547820c614f96503c5302
SMBuck <buckm2@g.cofc.edu>
2022-04-15 22:28:01 -0400

    hw 11 completed

--- assignments/hw11/sales_force.py
+++ assignments/hw11/sales_force.py
@@ -9,44 +9,32 @@ class SalesForce:
         for line in adding_people.readlines():
             strip = line.rstrip()
             splitter = strip.split(",")
-            employee_id = int(splitter[0])
-            name = splitter[1]
-            sales = splitter[2]
-            person = SalesPerson(employee_id, name), sales
-            self.sales_people.append(person)
+            sales_person = SalesPerson(splitter[0], splitter[1].lstrip())
+            sales_split = splitter[2].lstrip().rstrip().split(" ")
+            for sales in sales_split:
+                SalesPerson.enter_sale(sales_person, eval(sales))
+            self.sales_people.append(sales_person)
 
     def quota_report(self, quota):
-        my_data = open("sales_data.txt", "r")
-        float_list = []
-        for line in my_data.readlines():
-            strip = line.rstrip()
-            splitter = strip.split(",")
-            my_sum = ""
-            for value in splitter[2::3]:
-                my_sum += value
-                my_sum.lstrip()
-            new = my_sum.split(" ")
-            new.remove('')
-            for i in new:
-                float_list.append(float(i))
-        my_sum_1 = 0
-        for value in float_list[0:3]:
-           my_sum_1 += value
-        my_sum_2 = 0
-        for value in float_list[3:8]:
-            my_sum_2 += value
-        my_sum_3 = 0
-        for value in float_list[8:12]:
-            my_sum_3 += value
-        my_sum_4 = 0
-        for value in float_list[12:16]:
-            my_sum_4 += value
-        my_sum_5 = 0
-        for value in float_list[16:19]:
-            my_sum_5 += value
+        sales = []
+        for i in range(len(self.sales_people)):
+            employee = self.sales_people[i]
+            person = []
+            sales_id = SalesPerson.get_id(employee)
+            person.append(int(sales_id))
+            name = SalesPerson.get_name(employee)
+            person.append(name)
+            total_sales = float(SalesPerson.total_sales(employee))
+            person.append(total_sales)
+            if SalesPerson.met_quota(employee, quota):
+                person.append(True)
+            else:
+                person.append(False)
+            sales.append(person)
+        return sales
 
     def top_seller(self):
-
+        pass
 
     def individual_sales(self, employee_id):
         if employee_id in self.sales_people:
@@ -54,4 +42,5 @@ class SalesForce:
         return None
 
     def get_sale_frequencies(self):
+        pass
 
--- assignments/hw11/sales_person.py
+++ assignments/hw11/sales_person.py
@@ -3,10 +3,10 @@ class SalesPerson:
     def __init__(self, employee_id, name):
         self.employee_id = int(employee_id)
         self.name = name
-        self.sales_list = []
+        self.sales = []
 
     def get_id(self):
-        return int(self.employee_id)
+        return self.employee_id
 
     def get_name(self):
         return self.name
@@ -14,17 +14,17 @@ class SalesPerson:
     def set_name(self, name):
         self.name = name
 
-    def enter_sale(self, sale):
-        self.sales_list.append(float(sale))
+    def enter_sale(self, sales):
+        self.sales.append(sales)
 
     def total_sales(self):
         my_sum = 0
-        for sale in self.sales_list:
-            my_sum += sale
+        for sales in self.sales:
+            my_sum += sales
         return my_sum
 
     def get_sales(self):
-        return self.sales_list
+        return self.sales
 
     def met_quota(self, quota):
         if self.total_sales() >= quota:
@@ -32,11 +32,12 @@ class SalesPerson:
         return False
 
     def compare_to(self, other):
-        if other.total_sales() > self:
-            return -1
-        elif other.total_sales() < self:
+        if self.total_sales() > SalesPerson.total_sales(other):
             return 1
-        return 0
+        if self.total_sales() < SalesPerson.total_sales(other):
+            return -1
+        if self.total_sales() == SalesPerson.total_sales(other):
+            return 0
 
     def __str__(self):
         return '{}-{}:{}'.format(self.employee_id, self.name, self.total_sales())
